3e058e1250d678c47b6dfb62fc7202dff836eff5
Rachel BANDIERA <rachel.bandiera@gmail.com>
2020-08-31 16:59:30 +0200

    TP02

---
+++ restaurant-console-app/src/main/java/dev/AppSpringJava.java
@@ -0,0 +1,33 @@
+package dev;
+
+import dev.config.AppConfig;
+import dev.dao.PlatDaoFichier;
+import dev.dao.PlatDaoMemoire;
+import dev.ihm.Menu;
+import dev.service.PlatServiceVersion1;
+import dev.service.PlatServiceVersion2;
+
+import java.util.Scanner;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class AppSpringJava 
+{
+  public static void main(String[] args) 
+  {
+	  // Création du contexte Spring à partir d'une configuration Java
+	  AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+	  
+	  // récupération du bean Menu
+	  Menu menu = context.getBean(Menu.class);
+	  menu.afficher();
+	  
+	  // fermeture du Scanner
+	  context.getBean(Scanner.class).close();
+	  
+	  // fermeture du contexte Spring
+	  context.close();
+  }
+}
